bb0b5a627d55f003e7e4c3f668b7aec9893bc83b
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2024-05-16 17:22:54 -0500

    se cambia

--- backend/src/controller/controller.pets.js
+++ backend/src/controller/controller.pets.js
@@ -1,26 +1,29 @@
 import { Conexion } from "../database/conexion.js";
 import  multer from"multer";
 import { validationResult } from "express-validator";
-import fs from"node:fs"
+
+
+const storage= multer.diskStorage({
+    destination:function(req, file, cb ){
+        cb(null, './public/img')
+    },
+    filename:function(req, file, cb ){
+cb(null, file.originalname)
+    }
+})
+
+const upload= multer({storage:storage})
+
+export const saveimg=upload.single('foto')
 
 export const listar_pets= async(req, res)=>{
     try {
-        console.log(req.file);
-        const { id, raza, categoria_id, genero_id, id_dueño } = req.body;
-        const foto = req.file;
+        const [listar] = await Conexion.query("select*from mascotas");
 
-        // Verificar si hay un archivo adjunto
-        if (!foto) {
-            return res.status(400).json({ mensaje: "No se ha adjuntado ningún archivo." });
-        }
-
-        const fotos = saveimg(foto);
-        const [listar] = await Conexion.query("insert into mascotas(id,raza,categoria_id,foto,genero_id,id_dueño)values(?,?,?,?,?,?)", [id, raza, categoria_id, fotos, genero_id, id_dueño]);
-
-        if (listar.affectedRows > 0) {
-            res.status(200).json({ mensaje: "se crea co exito" });
+        if (listar.length > 0) {
+            res.status(200).json({ mensaje:listar});
         } else {
-            res.status(404).json({ mensaje: "no se creo la mascota " });
+            res.status(404).json({ mensaje: "no se encontraron  mascotas registradas"});
         }
     } catch (error) {
         console.log(error);
@@ -29,40 +32,21 @@ export const listar_pets= async(req, res)=>{
 
 }
 
-
-//se crea el directorio
- export  const upload = multer({dest:"./public/img"})
-//se guarda con el nombre oriinal
- function saveimg(file){
-    const newfile=`./public/img/${file.originalname}`
-    fs.renameSync(file.path,newfile);
-    return newfile
-}
-
 export const crear_pets = async (req, res) => {
     try {
+       const {raza, categoria_id, genero_id, id_amo}=req.body;
+       const {foto} = req.file.originalname;
+       const regiterpets= await Conexion.query("insert into mascotas(raza, categoria_id,foto,genero_id,id_amo)values(?,?,?,?,?)",[raza,categoria_id,foto,genero_id,id_amo])
+       
 
-        console.log(req.file);
-        const { raza, categoria_id, genero_id, id_dueño } = req.body;
-        const foto = req.file;
-        console.log("Raza:", raza);
-        console.log("Categoria ID:", categoria_id);
-        console.log("Genero ID:", genero_id);
-        console.log("ID Dueño:", id_dueño);
-
-        if (!foto) {
-            return res.status(400).json({ mensaje: "No se ha adjuntado ningún archivo." });
-        }
-        //se pasa la const que tiene el archivo
-        const fotos = saveimg(foto);
-
-        const [listar] = await Conexion.query("insert into mascotas(raza, categoria_id, foto, genero_id, id_dueño) values (?, ?, ?, ?, ?)", [raza, categoria_id, fotos, genero_id, id_dueño]);
-
-        if (listar.affectedRows > 0) {
-            res.status(200).json({ mensaje: "Se creó con éxito." });
-
-        } else {
-            res.status(404).json({ mensaje: "No se creó la mascota." });
+       if (regiterpets.affectedRows>0) {
+        return res.status(200).json({
+            "mensaje":"se creo con exito"
+        })
+        }else{
+            res.status(404).json({
+                "mensaje":"no se creo con exito"
+            })
         }
     } catch (error) {
         console.log(error);
--- backend/src/router/router.pets.js
+++ backend/src/router/router.pets.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { actualizar_pets, crear_pets, eliminar_pets, listar_pets } from "../controller/controller.pets.js";
-import { upload } from "../controller/controller.pets.js";
+import { actualizar_pets, crear_pets, eliminar_pets, listar_pets, saveimg } from "../controller/controller.pets.js";
 import { validator_create } from "../middlewares/middleware.pets.js";
 
 const ruta_pets= Router();
 
-ruta_pets.use(upload.single('foto')); 
+
 ruta_pets.get("/listar_pets",listar_pets);
-ruta_pets.post("/crear_pets",validator_create, crear_pets)
+ruta_pets.post("/crear_pets",saveimg, crear_pets)
 ruta_pets.put("/actualizar_pets/:id",actualizar_pets)
 ruta_pets.delete("/eliminar_pets/:id",eliminar_pets)
 
